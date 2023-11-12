734c3fcb05fb1f7cb96a6823f7a2a4a49028ebdc
Martin <123519281+martinkongm@users.noreply.github.com>
2023-11-12 10:33:57 -0500

    first commit

---
+++ src/context/TaskContext.jsx
@@ -0,0 +1,43 @@
+import { createContext, useState, useEffect } from 'react';
+import { tasks as data } from '../data/tasks.js';
+
+export const TaskContext = createContext();
+
+export function TaskContextProvider(props) {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
+  function createTask(task) {
+    setTasks([
+      ...tasks,
+      {
+        title: task.title,
+        id: tasks.length,
+        description: task.description,
+      },
+    ]);
+  }
+
+  function deleteTask(taskId) {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  }
+
+  return (
+    <>
+      <TaskContext.Provider
+        value={{
+          tasks,
+          createTask,
+          deleteTask,
+        }}
+      >
+        {props.children}
+      </TaskContext.Provider>
+    </>
+  );
+}
+
+export default TaskContext;
